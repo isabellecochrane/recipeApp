eef7b19924893acdb30db8a4066610cca7504cf3
Izzo <isabelle_cochrane@hotmail.co.uk>
2022-03-09 15:36:49 +0000

    adding a new recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,4 +6,23 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:title, :recipe)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root "recipes#index"
 
-  get "/recipes", to: "recipes#index"
-  get "/recipes/:id", to: "recipes#show"
+  resources :recipes
 end
